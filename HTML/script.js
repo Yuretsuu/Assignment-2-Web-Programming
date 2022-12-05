5c0f1253ab16184d48c038bb8e621d7ef475e0b6
fawazmezher <fawazmezher@hotmail.com>
2022-12-04 22:37:57 -0500

    navbar

--- HTML/script.js
+++ HTML/script.js
@@ -19,19 +19,6 @@
         $(window).resize(toggleNavbarMethod);
     });*/
 
-    const navBar = document.querySelector(".nav");
-    let scrollY = window.scrollY;
-
-    window.addEventListener("scroll", () => {
-        if (scrollY <= window.scrollY) {
-            navBar.classList.remove("nav-disappear");
-        } else {
-            navBar.classList.add("nav-disappear");
-        }
-
-        scrollY = window.scrollY;
-        }
-    );
 
 
 /* // Back to top button
